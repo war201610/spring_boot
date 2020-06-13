8e5a3678b506692b3cb679d093a6657572230155
远山 周 <1732559983@qq.com>
2020-06-13 18:26:08 +0800

    第三章 完结
    完成禁用模板缓存和配置日志, 完成3.2.2应用程序Bean的配置外置,
    完成3.3定制应用程序错误页面, 但是我还没验证错误页面是否成功. 把使用profile进行配置跳过了

--- src/main/java/readinglist/ReadingListController.java
+++ src/main/java/readinglist/ReadingListController.java
@@ -3,6 +3,8 @@ package readinglist;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,12 +14,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 //SpringMVC控制器
 @Controller
 @RequestMapping("/readingList")
+//@EnableConfigurationProperties//让下面的注解生效, 有无都可以, 奇怪之前这个amazonID属性一直显示为空
+//@ConfigurationProperties(prefix="amazon")
 public class ReadingListController {
+	
+	private AmazonProperties amazonProperties;
+	
 	private ReadingListRepository readingListRepository;
 
 	@Autowired
-	public ReadingListController(ReadingListRepository readingListRepository) {
+	public ReadingListController(ReadingListRepository readingListRepository, 
+			AmazonProperties amazonProperties) {
 		this.readingListRepository = readingListRepository;
+		this.amazonProperties = amazonProperties;
 	}
 
 	@RequestMapping(value = "/{reader}", method = RequestMethod.GET)
@@ -26,6 +35,7 @@ public class ReadingListController {
 		if (readingList != null) {
 			model.addAttribute("books", readingList);
 			model.addAttribute("reader", reader);
+			model.addAttribute("amazonID", amazonProperties.getAssociateId());
 		}
 		return "readingList";
 	}
